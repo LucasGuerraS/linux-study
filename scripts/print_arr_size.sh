62f733324d90b88b4a300ed30bd6f5a04fa007a1
LucasGuerraS <lucasgasardo@gmail.com>
2025-09-18 22:05:07 -0300

    chore: add documentation to rainbow script

--- scripts/print_arr_size.sh
+++ scripts/print_arr_size.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script prints the size of an array and a specific color based on the provided index.
+# Usage: ./print_arr_size.sh <index>
+
 INDEX=$1
 
 RAINBOW_COLORS=("Violet" "Indigo" "Blue" "Green" "Yellow" "Orange" "Red")

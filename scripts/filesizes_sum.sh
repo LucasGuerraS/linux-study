475a56dda618f6aae0ce9708ef07e3563943e949
LucasGuerraS <lucasgasardo@gmail.com>
2025-09-18 22:32:44 -0300

    feat: add macos specific file size and sum calculator script

---
+++ scripts/filesizes_sum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script takes two file paths as arguments and prints their individual sizes
+# in bytes, as well as the total size.
+# NOTE: This script is designed for macOS/BSD systems only (uses stat -f%z)
+
+FILE_SIZE_1=$(stat -f%z "$1")
+FILE_SIZE_2=$(stat -f%z "$2")
+
+echo "File size of $1 is: $FILE_SIZE_1"
+echo "File size of $2 is: $FILE_SIZE_2"
+
+TOTAL_SIZE=$(($FILE_SIZE_1 + $FILE_SIZE_2))
+
+echo "Total size is: $TOTAL_SIZE"
